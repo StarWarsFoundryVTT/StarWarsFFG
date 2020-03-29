2e5cdd8795c0eb0d2ec6b711fb34f3c073b5de87
Jaxxa <jaxxa90@gmail.com>
2020-03-29 19:16:18 +1100

    Setup Based on Simple System

---
+++ module/simple.js
@@ -0,0 +1,32 @@
+/**
+ * A simple and flexible system for world-building using an arbitrary collection of character and item attributes
+ * Author: Atropos
+ * Software License: GNU GPLv3
+ */
+
+// Import Modules
+import { SimpleItemSheet } from "./item-sheet.js";
+import { SimpleActorSheet } from "./actor-sheet.js";
+
+/* -------------------------------------------- */
+/*  Foundry VTT Initialization                  */
+/* -------------------------------------------- */
+
+Hooks.once("init", async function() {
+  console.log(`Initializing Simple Worldbuilding System`);
+
+	/**
+	 * Set an initiative formula for the system
+	 * @type {String}
+	 */
+	CONFIG.Combat.initiative = {
+	  formula: "1d20",
+    decimals: 2
+  };
+
+  // Register sheet application classes
+  Actors.unregisterSheet("core", ActorSheet);
+  Actors.registerSheet("dnd5e", SimpleActorSheet, { makeDefault: true });
+  Items.unregisterSheet("core", ItemSheet);
+  Items.registerSheet("dnd5e", SimpleItemSheet, {makeDefault: true});
+});
