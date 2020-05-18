1825fe0edece3d3f157999747615c65110ff2d01
Esrin <matt@purplejuice.co.uk>
2020-05-18 13:12:11 +0100

    A very hacky method has been introduced to allow the built in FoundryVTT combat tracker to roll initiative using FFG dice results. The resulting number is made up of successes and advantages. For example 1 success and 2 advantage would result in 1.02 for the initiative tracker. Warning, there might be bugs with this solution!
    Initiative can be switched between Vigilance and Cool via the System Settings section of the world configuration.

---
+++ modules/combat-ffg.js
@@ -0,0 +1,55 @@
+/**
+ * Extend the base Combat entity.
+ * @extends {Combat}
+ */
+export class CombatFFG extends Combat {
+
+  /** @override */
+  _getInitiativeRoll(combatant, formula) {
+    const cData = combatant.actor.data.data;
+    const origFormula = formula;
+
+    if (combatant.actor.data.type === "vehicle") { return new Roll("0"); }
+
+    if (formula === "Vigilance") {
+      formula = _getInitiativeFormula(cData.skills.Vigilance.rank, cData.characteristics.Willpower.value, 0);
+    }
+    else if (formula === "Cool") {
+      formula = _getInitiativeFormula(cData.skills.Cool.rank, cData.characteristics.Presence.value, 0);
+    }
+
+    const rollData = combatant.actor ? combatant.actor.getRollData() : {};
+    const letters = formula.split('');
+    const rolls = [];
+    const getSuc = new RegExp('Successes: ([0-9]+)', 'g');
+    const getAdv = new RegExp('Advantages: ([0-9]+)', 'g');
+
+    for (const letter of letters) {
+      rolls.push(game.ffg.StarWars.letterToRolls(letter, 1));
+    }
+
+    let newformula = combineAll(rolls, game.ffg.StarWars.rollValuesMonoid);
+
+    let rolling = game.specialDiceRoller.starWars.roll(newformula);
+
+    let results = game.specialDiceRoller.starWars.formatRolls(rolling);
+
+    let success = 0;
+    let advantage = 0;
+
+    success = getSuc.exec(results);
+    if (success) { success = success[1]; }
+    advantage = getAdv.exec(results);
+    if (advantage) { advantage = advantage[1]; }
+
+    let total = +success+(advantage*0.01);
+
+    console.log(`Total is: ${total}`);
+
+    let roll = new Roll(origFormula, rollData).roll();
+    roll._result = total;
+    roll._total = total;
+
+    return roll;
+  }
+}
