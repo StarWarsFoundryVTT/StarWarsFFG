c8e7cb7bec2707fec8b7607cfde8810f239da1e1
Esrin <matt@purplejuice.co.uk>
2020-06-07 15:00:03 +0100

    Added simple node tool to help translators check their language files for missing keys.

---
+++ lang/checkdiff.js
@@ -0,0 +1,13 @@
+// A simple node tool to help translators check for any missing keys in a particular language file vs en.json
+// Set the path of the 'checklang' variable to whichever language you want to compare with en.json
+// Then from terminal run: node checkdiff.js
+
+const enlang = require("./en.json");
+
+const checklang = require("./de.json");
+
+Object.keys(enlang).forEach((key) => {
+  if (!(key in checklang)) {
+    console.log(`Missing: ${key}`);
+  }
+});
