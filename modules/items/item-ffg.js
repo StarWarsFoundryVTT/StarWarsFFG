0195148bff9bc0422f60ed0a2bb46a4f0803d004
Chris Stadther <c.stadther@gmail.com>
2020-06-12 22:43:21 -0400

    Added specialization trees.

--- modules/items/item-ffg.js
+++ modules/items/item-ffg.js
@@ -42,6 +42,10 @@ export class ItemFFG extends Item {
     if(this.type === "forcepower") {
       this._prepareForcePowers();
     }
+
+    if(this.type === "specialization") {
+      this._prepareSpecializations();
+    }
   }
   /**
    * Capitalize string
@@ -52,97 +56,89 @@ export class ItemFFG extends Item {
     return s.charAt(0).toUpperCase() + s.slice(1);
   }
 
+  _prepareTalentTrees(collection, itemType, listProperty, hasGlobalList) {
+    const itemData = this.data;
+    const talents = itemData.data[collection];
+    let rowcount = 0;
+    const controls = Object.keys(talents).filter(item => {
+      return item.includes(itemType);
+    });
+
+    let itemList = [];
+    
+    for (let upgrade of controls) { 
+      if(upgrade.includes(itemType)) {
+        if(talents[upgrade].islearned) {
+          const item = talents[upgrade];
+          item.rank = 1;
+
+          let index = itemList.findIndex(obj => {
+            return obj.name === item.name;
+          });
+
+          if (index < 0) {
+            itemList.push(item);
+          } else {
+            itemList[index].rank += 1;
+          }
+        }
+
+        if(talents[upgrade].visible) {
+          if(!talents[upgrade].size || talents[upgrade].size === "single" ) {
+            talents[upgrade].size = "single";
+            talents[upgrade].canSplit = false;
+            rowcount += 1;
+          } else if(talents[upgrade].size === "double") {
+            rowcount += 2;
+            talents[upgrade].canSplit = true;
+          } else if(talents[upgrade].size === "triple") {
+            rowcount += 3;
+            talents[upgrade].canSplit = true;
+          } else {
+            rowcount += 4;
+            talents[upgrade].canSplit = true;
+          }
+        }
+
+        talents[upgrade].canCombine = false;
+
+        if(typeof talents[upgrade].visible === "undefined") {
+          talents[upgrade].visible = true;
+        }
+
+        talents[upgrade].canLinkTop = true;
+        talents[upgrade].canLinkRight = true;
+
+        if(rowcount === 4) {
+          talents[upgrade].canLinkRight = false;
+        }
+
+        const controlNumber = parseInt(upgrade.replace(itemType, ""), 10);
+        
+        if(rowcount < 4) {
+          talents[upgrade].canCombine = true;
+        } else {
+          rowcount = 0;
+        }
+      }
+    }
+
+    itemList.sort((a, b) => {
+      return a.name - b.name;
+    });
+
+    itemData[listProperty] = itemList;
+  }
+
+
+  _prepareSpecializations() {
+    this._prepareTalentTrees("talents", "talent", "talentList");
+  }
 
   /**
    * Prepare Force Power Item Data
    */
   _prepareForcePowers() {
-    const itemData = this.data;
-
-    const upgrades = itemData.data.upgrades;
-
-    let rowcount = 0;
-
-    const upgradeControls = Object.keys(upgrades).filter(item => {
-      return item.includes("upgrade");
-    })
-
-    let upgradeList = [];
-
-    for (let upgrade of upgradeControls) { 
-
-      if(upgrade.includes("upgrade")) {
-        if(upgrades[upgrade].islearned) {
-          const item = upgrades[upgrade];
-          item.rank = 1;
-
-          let index = upgradeList.findIndex(obj => {
-            return obj.name === item.name;
-          });
-
-          if (index < 0) {
-            upgradeList.push(item);
-          } else {
-            upgradeList[index].rank += 1;
-          }
-          
-        }
-        
-        if(upgrades[upgrade].visible) {
-          if(!upgrades[upgrade].size || upgrades[upgrade].size === "single" ) {
-            upgrades[upgrade].size = "single";
-            upgrades[upgrade].canSplit = false;
-            rowcount += 1;
-          } else if(upgrades[upgrade].size === "double") {
-            rowcount += 2;
-            upgrades[upgrade].canSplit = true;
-          } else if(upgrades[upgrade].size === "triple") {
-            rowcount += 3;
-            upgrades[upgrade].canSplit = true;
-          } else {
-            rowcount += 4;
-            upgrades[upgrade].canSplit = true;
-          }
-        }
-   
-        upgrades[upgrade].canCombine = false;
-   
-        if(typeof upgrades[upgrade].visible === "undefined") {
-          upgrades[upgrade].visible = true;
-        }
-
-        upgrades[upgrade].canLinkTop = true;
-        upgrades[upgrade].canLinkBottom = true;
-        upgrades[upgrade].canLinkLeft = true;
-        upgrades[upgrade].canLinkRight = true;
-        
-        if(rowcount === 0) {
-          upgrades[upgrade].canLinkLeft = false;
-        }
-
-        if(rowcount === 4) {
-          upgrades[upgrade].canLinkRight = false;
-        }
-
-        const controlNumber = parseInt(upgrade.replace("upgrade", ""), 10);
-     
-        if(controlNumber >= (upgradeControls.length - 4)) {
-          upgrades[upgrade].canLinkBottom = false;
-        }
-
-        if(rowcount < 4) {
-          upgrades[upgrade].canCombine = true;
-        } else {
-          rowcount = 0;
-        }
-
-      }
-    }
-
-    upgradeList.sort((a, b) => {
-      return a.name - b.name;
-    });
-
-    itemData.powerUpgrades = upgradeList;
+    this._prepareTalentTrees("upgrades", "upgrade", "powerUpgrades");
   }
 }
