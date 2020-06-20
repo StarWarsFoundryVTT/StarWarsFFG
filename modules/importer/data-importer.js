436f3a9491b682b6603c5f242e92a6866a66055e
Chris Stadther <c.stadther@gmail.com>
2020-06-20 19:22:22 -0400

    Force powers import (#133)
    * Started with force powers import.  Current issue is the large items are not hiding correctly.
    
    * fixed issue where spanned items were not hidden correctly
    fixed issue with box linking.
    
    * updated change log

--- modules/importer/data-importer.js
+++ modules/importer/data-importer.js
@@ -28,6 +28,8 @@ export default class DataImporter extends FormApplication {
       return decodeURIComponent(file);
     })
 
+    $(".import-progress").addClass("import-hidden");
+
     return {
       data,
       files,
@@ -67,6 +69,7 @@ export default class DataImporter extends FormApplication {
         .then(JSZip.loadAsync);                           
 
         this._enableImportSelection(zip.files, "Talents");
+        this._enableImportSelection(zip.files, "Force Abilities");
         // this._enableImportSelection(zip.files, "Gear");
         // this._enableImportSelection(zip.files, "Weapons");
         // this._enableImportSelection(zip.files, "Armor");
@@ -78,10 +81,7 @@ export default class DataImporter extends FormApplication {
 
     if(action === "import") {
       console.log('Starwars FFG - Importing Data Files');
-      $(".import-progress").toggleClass("import-hidden");
-
-      let totalCount = 0
-      let currentCount = 0;
+      
       const importFiles = $("input:checkbox[name=imports]:checked").map(function(){return { file : $(this).val(), label : $(this).data("name"), type : $(this).data("type"), itemtype : $(this).data("itemtype") } }).get()
 
       const selectedFile = $("#import-file").val();
@@ -96,82 +96,13 @@ export default class DataImporter extends FormApplication {
       .then(JSZip.loadAsync); 
 
       await this.asyncForEach(importFiles, async file => {
-        
-        let pack = game.packs.find(p => p.collection === `oggdude.${file.label}`);
-
-        if(!pack) {
-          pack = await Compendium.create({ entity : file.type, label: `oggdude.${file.label}`});
-        }
-
         const data = await zip.file(file.file).async("text");
 
         const parser = new DOMParser();
         const xmlDoc = parser.parseFromString(data,"text/xml");
 
-        // Handle import or talents
-        const talents = xmlDoc.getElementsByTagName("Talent");
-        totalCount += talents.length;
-        for(let i = 0; i < talents.length; i+=1) {
-          const talent = talents[i];
-          const importkey = talent.getElementsByTagName("Key")[0]?.textContent;
-          const name = talent.getElementsByTagName("Name")[0]?.textContent;
-          const description = talent.getElementsByTagName("Description")[0]?.textContent;
-          const ranked = talent.getElementsByTagName("Ranked")[0]?.textContent;
-
-          let activation = "Passive";
-          
-          switch (talent.getElementsByTagName("Activation")[0]?.textContent) {
-            case "Maneuver":
-              activation = "Active (Maneuver)";
-              break;
-            case "Action":
-              activation = "Active (Action)";
-              break;
-            case "Incidental":
-              activation = "Active (Incidental)";
-              break;
-            case "OOT Incidental":
-              activation = "Active (Incidental, Out of Turn)";
-              break;
-            default: 
-              activation = "Passive";
-          }
-
-          const forcetalent = talent.getElementsByTagName("ForceTalent")[0]?.textContent ? true : false;
-
-          const item = {
-            importkey,
-            name,
-            type: file.itemtype,
-            data : {
-              description,
-              ranks: {
-                ranked
-              },
-              activation : {
-                value : activation
-              },
-              isForceTalent : forcetalent
-            }
-          }
-
-          let compendiumItem;
-          await pack.getIndex();
-          let entry = pack.index.find(e => e.name === item.name);
-
-          if(!entry) {
-            console.debug(`Starwars FFG - Importing ${file.itemtype} ${file.type} - ${item.name}`);
-            if(file.type === "Item") {
-              compendiumItem = new Item(item);  
-            }
-
-            pack.importEntity(compendiumItem);
-          } 
-          currentCount +=1 ;
-          
-          $(".import-progress-bar").width(`${Math.trunc((currentCount / totalCount) * 100)}%`).html(`<span>${Math.trunc((currentCount / totalCount) * 100)}%</span>`);
-        }
-
+        await this._handleTalents(xmlDoc);
+        await this._handleForcePowers(xmlDoc, zip);
       });
 
       this.close();
@@ -209,10 +140,226 @@ export default class DataImporter extends FormApplication {
     
   }
 
+  async _handleTalents(xmlDoc) {
+    
+    const talents = xmlDoc.getElementsByTagName("Talent");
+    if(talents.length > 0) {
+      let totalCount = talents.length;
+      let currentCount = 0;
+
+      $(".import-progress.talents").toggleClass("import-hidden");
+      let pack = await this._getCompendiumPack('Item', `oggdude.Talents`);
+
+      for(let i = 0; i < talents.length; i+=1) {
+        const talent = talents[i];
+        const importkey = talent.getElementsByTagName("Key")[0]?.textContent;
+        const name = talent.getElementsByTagName("Name")[0]?.textContent;
+        const description = talent.getElementsByTagName("Description")[0]?.textContent;
+        const ranked = talent.getElementsByTagName("Ranked")[0]?.textContent;
+  
+        let activation = "Passive";
+        
+        switch (talent.getElementsByTagName("Activation")[0]?.textContent) {
+          case "Maneuver":
+            activation = "Active (Maneuver)";
+            break;
+          case "Action":
+            activation = "Active (Action)";
+            break;
+          case "Incidental":
+            activation = "Active (Incidental)";
+            break;
+          case "OOT Incidental":
+            activation = "Active (Incidental, Out of Turn)";
+            break;
+          default: 
+            activation = "Passive";
+        }
+  
+        const forcetalent = talent.getElementsByTagName("ForceTalent")[0]?.textContent ? true : false;
+  
+        const item = {
+          importkey,
+          name,
+          type: "talent",
+          data : {
+            description,
+            ranks: {
+              ranked
+            },
+            activation : {
+              value : activation
+            },
+            isForceTalent : forcetalent
+          }
+        }
+  
+        let compendiumItem;
+        await pack.getIndex();
+        let entry = pack.index.find(e => e.name === item.name);
+  
+        if(!entry) {
+          console.debug(`Starwars FFG - Importing Talent - Item`);
+          compendiumItem = new Item(item);  
+          pack.importEntity(compendiumItem);
+        } 
+        currentCount +=1 ;
+        
+        $(".talents .import-progress-bar").width(`${Math.trunc((currentCount / totalCount) * 100)}%`).html(`<span>${Math.trunc((currentCount / totalCount) * 100)}%</span>`);
+      }
+    }
+    
+  }
+
+  async _handleForcePowers(xmlDoc, zip) {
+    
+    const forceabilities = xmlDoc.getElementsByTagName("ForceAbility");
+
+    if(forceabilities.length > 0) {
+      
+      
+      $(".import-progress.force").toggleClass("import-hidden");
+      let pack = await this._getCompendiumPack('Item', `oggdude.ForcePowers`);
+
+      const fa = JXON.xmlToJs(xmlDoc)
+      console.log(fa);
+
+      // now we need to loop through the files in the Force Powers folder
+
+      const forcePowersFiles = Object.values(zip.files).filter(file => {
+        return !file.dir && file.name.split('.').pop() === 'xml' && file.name.includes("/Force Powers/");
+      })
+
+      let totalCount = forcePowersFiles.length;
+      let currentCount = 0;
+
+      await this.asyncForEach(forcePowersFiles, async (file) => {
+        const data = await zip.file(file.name).async("text");
+        const domparser = new DOMParser();
+        const xmlDoc1 = domparser.parseFromString(data,"text/xml");
+        const fp = JXON.xmlToJs(xmlDoc1);
+
+        console.log(fp);
+        // setup the base information
+
+        let power = {
+          name : fp.ForcePower.Name,
+          type : "forcepower",
+          data : {
+            upgrades : {
+
+            }
+          }
+        }
+
+        // get the basic power informatio
+        const importKey = fp.ForcePower.AbilityRows.AbilityRow[0].Abilities.Key[0];
+
+        let forceAbility = fa.ForceAbilities.ForceAbility.find(ability => {
+          return ability.Key === importKey
+        })
+
+        power.data.description = forceAbility.Description;
+
+        // next we will parse the rows
+
+        for(let i = 1; i < fp.ForcePower.AbilityRows.AbilityRow.length; i+=1) {
+          const row = fp.ForcePower.AbilityRows.AbilityRow[i];
+          row.Abilities.Key.forEach((keyName, index) => {
+            let rowAbility = { }
+
+            let rowAbilityData = fa.ForceAbilities.ForceAbility.find(a => {
+              return a.Key === keyName;
+            })
+
+            rowAbility.name = rowAbilityData.Name;
+            rowAbility.description = rowAbilityData.Description;
+            rowAbility.cost = row.Costs.Cost[index];
+            rowAbility.visible = true;
+
+            if(row.Directions.Direction[index].Up) {
+              rowAbility["links-top-1"] = true;
+            }
+            
+            switch(row.AbilitySpan.Span[index]) {
+              case "1" :
+                rowAbility.size = "single";
+                break;
+              case "2" :
+                rowAbility.size = "double";
+                if(index < 3 && row.Directions.Direction[index+1].Up) {
+                  rowAbility["links-top-2"] = true;
+                }
+                break;
+              case "3" :
+                rowAbility.size = "triple";
+                if(index < 2 && row.Directions.Direction[index+1].Up) {
+                  rowAbility["links-top-2"] = true;
+                }
+                if(index < 2 && row.Directions.Direction[index+2].Up) {
+                  rowAbility["links-top-3"] = true;
+                }
+                break;
+              case "4": 
+                rowAbility.size = "full";
+                if(index < 1 && row.Directions.Direction[index+1].Up) {
+                  rowAbility["links-top-2"] = true;
+                }
+                if(index < 1 && row.Directions.Direction[index+2].Up) {
+                  rowAbility["links-top-3"] = true;
+                }
+                if(index < 1 && row.Directions.Direction[index+3].Up) {
+                  rowAbility["links-top-4"] = true;
+                }
+                break
+              default:
+                rowAbility.size = "single";
+                rowAbility.visible = false;
+            }
+            
+            if(row.Directions.Direction[index].Right) {
+              rowAbility["links-right"] = true;
+            }
+
+            const talentKey = `upgrade${((i - 1) * 4) + index}`;
+            power.data.upgrades[talentKey] = rowAbility;
+          });
+        }
+
+        let compendiumItem;
+        await pack.getIndex();
+        let entry = pack.index.find(e => e.name === power.name);
+  
+        if(!entry) {
+          console.debug(`Starwars FFG - Importing Force Power - Item`);
+          compendiumItem = new Item(power);  
+
+          console.log(compendiumItem);
+
+          pack.importEntity(compendiumItem);
+        } else {
+          console.debug(`Starwars FFG - Updating Force Power - Item`);
+        }
+        currentCount +=1 ;
+        
+        $(".force .import-progress-bar").width(`${Math.trunc((currentCount / totalCount) * 100)}%`).html(`<span>${Math.trunc((currentCount / totalCount) * 100)}%</span>`);
+      });
+    }
+  }
+
+  async _getCompendiumPack(type, name) {
+    let pack = game.packs.find(p => p.collection === name);
+    if(!pack) {
+      pack = await Compendium.create({ entity : type, label: name});
+    }
+
+    return pack;
+  }
+
   _enableImportSelection(files, name) {
     Object.values(files).findIndex(file => {
       if(file.name.includes(`/${name}.xml`)) {
-        $(`#import${name}`).removeAttr("disabled").val(file.name);
+        $(`#import${name.replace(" ", "")}`).removeAttr("disabled").val(file.name);
         return true;
       }
       return false;
