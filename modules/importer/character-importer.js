89b3f32db6854fba6fc8ba20b51fa7d53ca2b6f1
Chris Stadther <c.stadther@gmail.com>
2020-08-26 12:05:54 -0400

    Enhanced OggDude Character Import, moved to FormApplication, added progress bar, added additional error handling and logging.

---
+++ modules/importer/character-importer.js
@@ -0,0 +1,52 @@
+import ImportHelpers from "./import-helpers.js";
+
+export default class CharacterImporter extends FormApplication {
+  /** @override */
+  static get defaultOptions() {
+    return mergeObject(super.defaultOptions, {
+      id: "ogc-importer",
+      classes: ["starwarsffg", "data-import"],
+      title: "OggDude Character Importer",
+      width: 385,
+      height: 200,
+      template: "systems/starwarsffg/templates/importer/character-importer.html",
+    });
+  }
+
+  /**
+   * Return a reference to the target attribute
+   * @type {String}
+   */
+  get attribute() {
+    return this.options.name;
+  }
+
+  /** @override */
+  async getData() {
+    $(".import-progress").addClass("import-hidden");
+
+    if (!CONFIG?.temporary) {
+      CONFIG.temporary = {};
+    }
+
+    return {
+      cssClass: "data-importer-window",
+    };
+  }
+
+  /** @override */
+  activateListeners(html) {
+    super.activateListeners(html);
+
+    html.find(".dialog-button").on("click", async (event) => {
+      event.preventDefault();
+      event.stopPropagation();
+
+      const form = html[0];
+      if (!form.data.files.length) return ui.notifications.error("You did not upload a data file!");
+      const text = await readTextFromFile(form.data.files[0]);
+      await ImportHelpers.characterImport(text);
+      this.close();
+    });
+  }
+}
