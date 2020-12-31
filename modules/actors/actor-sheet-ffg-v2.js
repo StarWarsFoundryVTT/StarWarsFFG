1553ce26368f2e1f0ec04d0cc7f51d6602a044ae
Chris Stadther <c.stadther@gmail.com>
2020-12-30 19:20:41 -0500

    Refactored tabs to a handlebars partial template, so that all sheets use the same code to generate tab controls. (#577)
    Created v2 of all sheets (v2 sheets include right side tab control)

---
+++ modules/actors/actor-sheet-ffg-v2.js
@@ -0,0 +1,29 @@
+import { ActorSheetFFG } from "./actor-sheet-ffg.js";
+
+export class ActorSheetFFGV2 extends ActorSheetFFG {
+  constructor(...args) {
+    super(...args);
+  }
+
+  /** @override */
+  static get defaultOptions() {
+    return mergeObject(super.defaultOptions, {
+      classes: ["starwarsffg", "sheet", "actor", "v2"],
+      template: "systems/starwarsffg/templates/actors/ffg-character-sheet.html",
+      width: 710,
+      height: 650,
+      tabs: [{ navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "characteristics" }],
+      scrollY: [".tableWithHeader", ".tab", ".skillsGrid"],
+    });
+  }
+
+  getData() {
+    const data = super.getData();
+    return data;
+  }
+
+  /** @override */
+  activateListeners(html) {
+    super.activateListeners(html);
+  }
+}
