3f55fbf0c5744e6d572c463fc8021fb6305de249
Chris Stadther <c.stadther@gmail.com>
2021-01-07 19:46:40 -0500

    Added basis for item attachment and modifier as items for drag and drop.

--- modules/config/ffg-modifiers.js
+++ modules/config/ffg-modifiers.js
@@ -58,58 +58,6 @@ export const general_modifiers = {
 };
 
 export const weapon_modifiers = {
-  "Career Skill": {
-    "value": "Career Skill",
-    "label": "SWFFG.ModTypeCareerSkill",
-  },
-  "Characteristic": {
-    "value": "Characteristic",
-    "label": "SWFFG.ModTypeCharacteristic",
-  },
-  "Skill Add Advantage": {
-    "value": "Skill Add Advantage",
-    "label": "SWFFG.ModTypeSkillAddAdvantage",
-  },
-  "Skill Add Dark": {
-    "value": "Skill Add Dark",
-    "label": "SWFFG.ModTypeSkillAddDark",
-  },
-  "Skill Add Failure": {
-    "value": "Skill Add Failure",
-    "label": "SWFFG.ModTypeSkillAddFailure",
-  },
-  "Skill Add Light": {
-    "value": "Skill Add Light",
-    "label": "SWFFG.ModTypeSkillAddLight",
-  },
-  "Skill Add Success": {
-    "value": "Skill Add Success",
-    "label": "SWFFG.ModTypeSkillAddSuccess",
-  },
-  "Skill Add Threat": {
-    "value": "Skill Add Threat",
-    "label": "SWFFG.ModTypeSkillAddThreat",
-  },
-  "Skill Boost": {
-    "value": "Skill Boost",
-    "label": "SWFFG.ModTypeSkillBoost",
-  },
-  "Skill Rank": {
-    "value": "Skill Rank",
-    "label": "SWFFG.ModTypeSkillRank",
-  },
-  "Skill Remove Setback": {
-    "value": "Skill Remove Setback",
-    "label": "SWFFG.ModTypeSkillRemoveSetback",
-  },
-  "Skill Setback": {
-    "value": "Skill Setback",
-    "label": "SWFFG.ModTypeSkillSetback",
-  },
-  "Stat": {
-    "value": "Stat",
-    "label": "SWFFG.ModTypeStat",
-  },
   "Weapon Stat": {
     "value": "Weapon Stat",
     "label": "SWFFG.ModTypeStatWeapon",
@@ -120,5 +68,24 @@ export const vehicle_modifiers = {
   "Stat": {
     "value": "Stat",
     "label": "SWFFG.ModTypeStat",
+  },
+};
+
+export const modifier_types = {
+  "All": {
+    "value": "all",
+    "label": "SWFFG.ModTypeAll",
+  },
+  "Armor": {
+    "value": "armor",
+    "label": "SWFFG.ItemsArmor",
+  },
+  "Vehicle": {
+    "value": "vehicle",
+    "label": "SWFFG.ItemsVehicles",
+  },
+  "Weapon": {
+    "value": "weapon",
+    "label": "SWFFG.ItemsWeapons",
   },
 };
