08f44674272eade83928e7807b18c0945c8579d3
Chris Stadther <c.stadther@gmail.com>
2021-01-08 19:02:23 -0500

    can now add new item qualities (bypassing the item directory) by clicking on the box header

--- modules/items/itembase-ffg.js
+++ modules/items/itembase-ffg.js
@@ -6,10 +6,19 @@ export default class ItemBaseFFG extends Item {
       const realItem = await game.items.get(this.data.flags.ffgTempId);
 
       if (realItem) {
+        if (!this.data._id) {
+          this.data._id = randomID();
+        }
+
         const mergedData = { ...this.data.data, ...data.data };
         data.data = mergedData;
 
-        realItem.data.data[this.data.flags.ffgTempItemType][this.data.flags.ffgTempItemIndex] = { ...this.data, ...data, flags: {} };
+        if (this.data.flags.ffgTempItemIndex > -1) {
+          realItem.data.data[this.data.flags.ffgTempItemType][this.data.flags.ffgTempItemIndex] = { ...this.data, ...data, flags: {} };
+        } else {
+          this.data.flags.ffgTempItemIndex = realItem.data.data[this.data.flags.ffgTempItemType].length;
+          realItem.data.data[this.data.flags.ffgTempItemType].push({ ...this.data, ...data, flags: {} });
+        }
       }
 
       let formData = {};
