6ea93a8730f57b81e3eda0434e10468314311987
Chris Stadther <c.stadther@gmail.com>
2021-01-10 18:33:49 -0500

    switched from count for additional modifiers to active, and added update functionality

--- modules/items/itembase-ffg.js
+++ modules/items/itembase-ffg.js
@@ -7,67 +7,6 @@ export default class ItemBaseFFG extends Item {
     } else {
       await EmbeddedItemHelpers.updateRealObject(this, data);
       this.sheet.render(true);
-      // let flags = this.data.flags;
-
-      // let realItem = await game.items.get(flags.ffgTempId);
-      // let parents = [];
-
-      // if(realItem) {
-      //   parents.push(flags)
-      // } else {
-      //   let x = flags?.ffgParent
-
-      //   parents.push(x);
-
-      //   let ffgTempId = "";
-      //   while(x) {
-      //     if(Object.values(x.ffgParent).length > 0) {
-      //       parents.push(x.ffgParent);
-      //       x = x.ffgParent;
-      //     } else {
-      //       flags = x;
-      //       ffgTempId = x.ffgTempId;
-      //       x = undefined;
-      //     }
-      //   }
-
-      //   realItem = await game.items.get(ffgTempId);
-      // }
-
-      // if (realItem) {
-      //   if (!this.data._id) {
-      //     data._id = randomID();
-      //   }
-
-      //   const mergedData = { ...this.data.data, ...data.data };
-      //   data.data = mergedData;
-      //   const itemData = { ...this.data, ...data };
-
-      //   let dataPointer = realItem.data.data;
-
-      //   parents.forEach((value, index) => {
-      //     dataPointer = dataPointer[parents[index].ffgTempItemType][parents[index].ffgTempItemIndex]
-      //   });
-
-      //   if (this.data.flags.ffgTempItemIndex > -1) {
-      //     dataPointer.data[this.data.flags.ffgTempItemType][this.data.flags.ffgTempItemIndex] = { ...itemData, flags: {} };
-      //     //realItem.data.data[this.data.flags.ffgTempItemType][this.data.flags.ffgTempItemIndex] = { ...itemData, flags: {} };
-      //   } else {
-      //     this.data.flags.ffgTempItemIndex = dataPointer.data[this.data.flags.ffgTempItemType].length
-      //     dataPointer.data[this.data.flags.ffgTempItemType].push({ ...itemData, flags: {} });
-      //     //this.data.flags.ffgTempItemIndex = realItem.data.data[this.data.flags.ffgTempItemType].length;
-      //     //realItem.data.data[this.data.flags.ffgTempItemType].push({ ...itemData, flags: {} });
-      //   }
-
-      //   let formData = {};
-      //   setProperty(formData, `data.${parents[0].ffgTempItemType}`, realItem.data.data[parents[0].ffgTempItemType]);
-
-      //   this.data = itemData;
-
-      //   realItem.update(formData);
-      //   this.sheet.render(true);
-      //   return;
-      // }
     }
   }
 }
