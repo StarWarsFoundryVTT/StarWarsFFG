f9a7a055f47bf29fadd1c76b9f982d220f835793
Chris Stadther <c.stadther@gmail.com>
2021-01-11 22:24:41 -0500

    Add new modifier types for itemmodifiers that affect only rolls

--- modules/swffg-config.js
+++ modules/swffg-config.js
@@ -1,12 +1,13 @@
 import { itemstatus } from "./config/ffg-itemstatus.js";
 import { personal_ranges, vehicle_ranges, sensor_ranges } from "./config/ffg-ranges.js";
-import { general_modifiers, weapon_modifiers, vehicle_modifiers, modifier_types } from "./config/ffg-modifiers.js";
+import { general_modifiers, weapon_modifiers, vehicle_modifiers, modifier_types, itemmodifier_modifiertypes, itemmodifier_rollmodifiers, itemmodifier_resultmodifiers, itemmodifier_dicemodifiers } from "./config/ffg-modifiers.js";
 import { pool_results } from "./config/ffg-dice.js";
 import { vehicle_stats, vehicle_firingarcs } from "./config/ffg-vehicles.js";
 import { character_characteristics, character_stats } from "./config/ffg-characters.js";
 import { skills, skills_knowledge_stripped, skills_combat } from "./config/ffg-skills.js";
 import { sheet_defaults } from "./config/ffg-sheetdefaults.js";
 import { weapon_stats } from "./config/ffg-weapons.js";
+import { armor_stats } from "./config/ffg-armor.js";
 import { talent_activations } from "./config/ffg-talents.js";
 import { difficulty } from "./config/ffg-difficulty.js";
 
@@ -32,3 +33,8 @@ FFG.vehicle_stats = vehicle_stats;
 FFG.weapon_mod_types = weapon_modifiers;
 FFG.weapon_stats = weapon_stats;
 FFG.itemmodifier_types = modifier_types;
+FFG.itemmodifier_mod_types = itemmodifier_modifiertypes;
+FFG.itemmodifier_rollmodifiers = itemmodifier_rollmodifiers;
+FFG.itemmodifier_dicemodifiers = itemmodifier_dicemodifiers;
+FFG.itemmodifier_resultmodifiers = itemmodifier_resultmodifiers;
+FFG.armor_stats = armor_stats;
