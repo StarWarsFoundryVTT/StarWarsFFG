59fe8b93ab07b3307cb4c9d92071b25869eb7751
Chris Stadther <c.stadther@gmail.com>
2021-01-15 12:22:27 -0500

    Added handler for compendium items

--- modules/items/itembase-ffg.js
+++ modules/items/itembase-ffg.js
@@ -3,12 +3,19 @@ import EmbeddedItemHelpers from "../helpers/embeddeditem-helpers.js";
 export default class ItemBaseFFG extends Item {
   async update(data, options = {}) {
     if (!this.data?.flags?.ffgTempId) {
-      return super.update(data, options);
+      super.update(data, options);
+      if (this.compendium) {
+        return this.sheet.render(true);
+      }
+      return;
     } else {
       await EmbeddedItemHelpers.updateRealObject(this, data);
 
-      //this.sheet.render(false);
-      //return this;
+      if (this.data.flags?.ffgParent?.isCompendium) {
+      } else {
+        this.sheet.render(false);
+        return this;
+      }
     }
   }
 }
