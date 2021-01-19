e6237d44a0014d216c6a3b724c15f851bb17adf6
Chris Stadther <c.stadther@gmail.com>
2021-01-18 19:13:57 -0500

    Fixed issue where users could not display read-only journal entries, by setting default permissions to observer.

--- modules/items/journalentry-ffg.js
+++ modules/items/journalentry-ffg.js
@@ -3,6 +3,32 @@
  * @extends {Item}
  */
 export default class JournalEntryFFG extends JournalEntry {
+  /** @override */
+  static get config() {
+    return {
+      baseEntity: JournalEntry,
+      collection: game.journal,
+      embeddedEntities: {},
+      label: "ENTITY.JournalEntry",
+      permissions: {
+        create: "TEMPLATE_CREATE",
+      },
+    };
+  }
+
+  /** @override */
+  static get defaultOptions() {
+    return mergeObject(super.defaultOptions, {
+      classes: ["sheet", "journal-sheet"],
+      width: 720,
+      height: 800,
+      resizable: true,
+      closeOnSubmit: false,
+      submitOnClose: true,
+      viewPermission: ENTITY_PERMISSIONS.OBSERVER,
+    });
+  }
+
   async update(data, options = {}) {
     return;
   }
