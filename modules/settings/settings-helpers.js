fa5977e6f5b9e535d5ddb7b97cfb7b1fd6ab5aaf
Matt Stoolman <Esrin@users.noreply.github.com>
2022-01-06 21:00:26 +0000

    Merge dev to master - dev is going bye-bye (#990)
    * Get recent PRs into dev branch (#986)
    
    * Bump path-parse from 1.0.6 to 1.0.7 (#960)
    
    Bumps [path-parse](https://github.com/jbgutierrez/path-parse) from 1.0.6 to 1.0.7.
    - [Release notes](https://github.com/jbgutierrez/path-parse/releases)
    - [Commits](https://github.com/jbgutierrez/path-parse/commits/v1.0.7)
    
    ---
    updated-dependencies:
    - dependency-name: path-parse
      dependency-type: indirect
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Add Remove Setback as Roll Mod (#978)
    
    Adds a Roll Mod to "Remove Setback" (issue #916)
    
    Co-authored-by: Alan Landucci-Ruiz <eightballmclaw@gmail.com>
    
    * add data-item-id in chat rolls (#984)
    
    * add data-item-id to item chat rolls
    
    #970
    
    * add data-item-id to force-power-card chat template
    
    * add data-item-id to item-card.html chat template
    
    * add data-item-id to roll-forcepower-card template
    
    * fix talent expansion (#985)
    
    Compendium documents changed the `id` property to a private property.
    This code switches to using a CompendiumCollection method for getting
    the document.
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: Alan Landucci-Ruiz <MClaw666@yahoo.com>
    Co-authored-by: Alan Landucci-Ruiz <eightballmclaw@gmail.com>
    Co-authored-by: Tony <anthony@correa.co>
    Co-authored-by: Ben Heiskell <ben@xdxa.org>
    
    * Fix ui settings menu (#989)
    
    Fix UI settings manager dialog
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: Alan Landucci-Ruiz <MClaw666@yahoo.com>
    Co-authored-by: Alan Landucci-Ruiz <eightballmclaw@gmail.com>
    Co-authored-by: Tony <anthony@correa.co>
    Co-authored-by: Ben Heiskell <ben@xdxa.org>
    Co-authored-by: tropic <81570172+tropiclwd@users.noreply.github.com>

--- modules/settings/settings-helpers.js
+++ modules/settings/settings-helpers.js
@@ -202,6 +202,7 @@ export default class SettingsHelpers {
       type: UISettings,
       restricted: true,
     });
+
     game.settings.register("starwarsffg", "uiSettings", {
       name: "UI Settings",
       scope: "world",
@@ -212,6 +213,7 @@ export default class SettingsHelpers {
 
     // Register settings for UI Themes
     game.settings.register("starwarsffg", "ui-uitheme", {
+      module: "starwarsffg",
       name: game.i18n.localize("SWFFG.SettingsUITheme"),
       hint: game.i18n.localize("SWFFG.SettingsUIThemeHint"),
       scope: "world",
@@ -226,6 +228,7 @@ export default class SettingsHelpers {
     });
 
     game.settings.register("starwarsffg", "ui-pausedImage", {
+      module: "starwarsffg",
       name: game.i18n.localize("SWFFG.SettingsPausedImage"),
       hint: game.i18n.localize("SWFFG.SettingsPausedImageHint"),
       scope: "world",
