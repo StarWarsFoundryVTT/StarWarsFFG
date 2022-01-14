346a212004e6a87814b338c7ddc973698bbf174a
Wrycu <leetsaber@gmail.com>
2022-01-14 08:52:23 -0800

    fix(qualities): wrap fixing of data in error handling
    * also remove the extra console.log!

--- modules/dice/roll.js
+++ modules/dice/roll.js
@@ -279,16 +279,18 @@ export class RollFFG extends Roll {
         let test_item = game.actors.get(candidate_actors[0].id).items.get(chatData.data._id);
         // for whatever reason, sometimes the item we read doesn't have modifiers even though the chat item does
         // check if this is the case and correct it if it is
-        if (test_item.data?.data?.itemmodifier.length === 0 && chatData.data?.data?.itemmodifier) {
-          // there aren't any modifiers on the object, try copying the temp object to it so the link works
-          test_item.data.data.itemmodifier = chatData.data.data.itemmodifier;
+        try {
+          if (test_item.data?.data?.itemmodifier.length === 0 && chatData.data?.data?.itemmodifier) {
+            // there aren't any modifiers on the object, try copying the temp object to it so the link works
+            test_item.data.data.itemmodifier = chatData.data.data.itemmodifier;
+          }
+        } catch (exception) {
+          // required data was missing - best to just move along, citizen
         }
         // fake the UUID flag so we can do the lookup within chat messages
         chatData.data.flags.starwarsffg.ffgUuid = 'Actor.' + candidate_actors[0].id + '.Item.' + chatData.data._id;
       }
     }
-
-    console.log(chatData)
 
     // Render the roll display template
     return renderTemplate(chatOptions.template, chatData);
