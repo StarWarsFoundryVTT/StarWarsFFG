5fa9972120a89e4c47603f3b4c4bb91b2e8a3f16
Wrycu <tim@leetsaber.com>
2023-01-08 13:41:38 -0800

    fix(crew): use MOAR const for crew

--- modules/helpers/dice-helpers.js
+++ modules/helpers/dice-helpers.js
@@ -260,12 +260,12 @@ export default class DiceHelpers {
  * @returns {DicePoolFFG}
  */
 export function get_dice_pool(actor_id, skill_name, incoming_roll) {
-  let actor = game.actors.get(actor_id);
-  var parsed_skill_name = convert_skill_name(skill_name);
-  var skill = actor.system.skills[parsed_skill_name];
-  var characteristic = actor.system.characteristics[skill.characteristic];
+  const actor = game.actors.get(actor_id);
+  const parsed_skill_name = convert_skill_name(skill_name);
+  const skill = actor.system.skills[parsed_skill_name];
+  const characteristic = actor.system.characteristics[skill.characteristic];
 
-  let dicePool = new DicePoolFFG({
+  const dicePool = new DicePoolFFG({
     ability: (Math.max(characteristic.value, skill.rank) + incoming_roll.ability) - (Math.min(characteristic.value, skill.rank) + incoming_roll.proficiency),
     proficiency: Math.min(characteristic.value, skill.rank) + incoming_roll.proficiency,
     boost: skill.boost + incoming_roll.boost,
@@ -291,7 +291,7 @@ export function get_dice_pool(actor_id, skill_name, incoming_roll) {
  */
 function convert_skill_name(pool_skill_name) {
   CONFIG.logger.debug(`Converting ${pool_skill_name} to skill name`);
-  let skills = CONFIG.FFG.skills;
+  const skills = CONFIG.FFG.skills;
   for (var skill in skills) {
     if (skills[skill]['label'] === pool_skill_name) {
       CONFIG.logger.debug(`Found mapping to ${skill}`);
