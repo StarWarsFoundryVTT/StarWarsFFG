8b0ba575ee3e4ca9e0b8bce85b2905568c8d3014
Wrycu <tim@leetsaber.com>
2023-11-19 09:59:47 -0800

    chore(v11): cypress config and comment
    #1244

--- cypress.config.js
+++ cypress.config.js
@@ -3,12 +3,12 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
     e2e: {
         /**
-         * Override baseUrl if provided via the config.env.json
+         * Override baseUrl if provided via the cypress.env.json
          * https://stackoverflow.com/questions/47262338/overriding-configuration-variables-from-cypress-env-json
          */
         setupNodeEvents(on, config) {
             if (config.hasOwnProperty("env") && config.env.hasOwnProperty("baseUrl")) {
-                config.baseUrl = cypress.env.baseUrl;
+                config.baseUrl = config.env.baseUrl;
             }
             return config;
         },
