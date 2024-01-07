8a50d67dec74cdaa7d92a8d545ccc34404e71f83
ZolOnTheNet <103053124+ZolOnTheNet@users.noreply.github.com>
2024-01-07 23:44:19 +0100

    modif des competences
    modification des noms de comptence,
    ajout d'un formulaire pour l'acteur pour K² (en vu d'en faire un nouveau)

---
+++ modules/actors/actor-sheet-k2g-v2.js
@@ -0,0 +1,29 @@
+import { ActorSheetFFG } from "./actor-sheet-ffg.js";
+
+export class ActorSheetK2G extends ActorSheetFFG {
+  constructor(...args) {
+    super(...args);
+  }
+
+  /** @override */
+  static get defaultOptions() {
+    return mergeObject(super.defaultOptions, {
+      classes: ["k2G", "sheet", "actor", "v2"],
+      template: "systems/genesysk2/templates/actors/k2g-character-sheet.html",
+      width: 710,
+      height: 650,
+      tabs: [{ navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "characteristics" }],
+      scrollY: [".tableWithHeader", ".tab", ".skillsGrid", ".skillsTablesGrid"],
+    });
+  }
+
+  getData() {
+    const data = super.getData();
+    return data;
+  }
+
+  /** @override */
+  activateListeners(html) {
+    super.activateListeners(html);
+  }
+}
