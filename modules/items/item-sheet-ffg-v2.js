19d4bd2f9d9c43233d1dc104c5f4d229ace0521d
Wrycu <tim@leetsaber.com>
2024-05-22 21:11:18 -0700

    fix(v12): specialization
    * properly render dice image in specializations
    
    #1511

--- modules/items/item-sheet-ffg-v2.js
+++ modules/items/item-sheet-ffg-v2.js
@@ -3,7 +3,7 @@ import { ItemSheetFFG } from "./item-sheet-ffg.js";
 export class ItemSheetFFGV2 extends ItemSheetFFG {
   /** @override */
   static get defaultOptions() {
-    return mergeObject(super.defaultOptions, {
+    return foundry.utils.mergeObject(super.defaultOptions, {
       classes: ["starwarsffg", "sheet", "item", "v2"],
       tabs: [{ navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "description" }],
       scrollY: [".sheet-body", ".tab"],
