f2bba277b7c63af18bb98192da895d5e77b2f6a8
wrycu <4709746+wrycu@users.noreply.github.com>
2026-01-05 22:11:04 -0800

    fix(importer): motivations
    * add missing motivation types
    
    #1974

--- modules/config/ffg-character-creator.js
+++ modules/config/ffg-character-creator.js
@@ -45,32 +45,128 @@ const obligationTypes = {
 
 const motivationTypes = {
   "Ambition": {
-    "value": "ambition",
+    "value": "Ambition",
     "label": "SWFFG.CharacterCreator.Motivation.ambition",
   },
   "Belief": {
-    "value": "belief",
+    "value": "Belief",
     "label": "SWFFG.CharacterCreator.Motivation.belief",
   },
   "Cause": {
-    "value": "cause",
+    "value": "Cause",
     "label": "SWFFG.CharacterCreator.Motivation.cause",
   },
-  "Connection": {
-    "value": "connection",
+  "Connections": {
+    "value": "Connections",
     "label": "SWFFG.CharacterCreator.Motivation.connection",
   },
   "Faith": {
-    "value": "faith",
+    "value": "Faith",
     "label": "SWFFG.CharacterCreator.Motivation.faith",
   },
   "Quest": {
-    "value": "quest",
+    "value": "Quest",
     "label": "SWFFG.CharacterCreator.Motivation.quest",
   },
   "Relationship": {
-    "value": "relationship",
+    "value": "Relationship",
     "label": "SWFFG.CharacterCreator.Motivation.relationship",
+  },
+  "Discovery": {
+    "value": "Discovery",
+    "label": "SWFFG.CharacterCreator.Motivation.discovery",
+  },
+  "Conflict": {
+    "value": "Conflict",
+    "label": "SWFFG.CharacterCreator.Motivation.conflict",
+  },
+  "Creations": {
+    "value": "Creation",
+    "label": "SWFFG.CharacterCreator.Motivation.creation",
+  },
+  "Reward": {
+    "value": "Reward",
+    "label": "SWFFG.CharacterCreator.Motivation.reward",
+  },
+  "Creed": {
+    "value": "Creed",
+    "label": "SWFFG.CharacterCreator.Motivation.creed",
+  },
+  "Oath": {
+    "value": "Oath",
+    "label": "SWFFG.CharacterCreator.Motivation.oath",
+  },
+  "Drive": {
+    "value": "Drive",
+    "label": "SWFFG.CharacterCreator.Motivation.drive",
+  },
+  "Location: Aleen": {
+    "value": "Aleen",
+    "label": "SWFFG.CharacterCreator.Motivation.aleen",
+  },
+  "Location: Auratera": {
+    "value": "Auratera",
+    "label": "SWFFG.CharacterCreator.Motivation.auratera",
+  },
+  "Location: Bardotta": {
+    "value": "Bardotta",
+    "label": "SWFFG.CharacterCreator.Motivation.bardotta",
+  },
+  "Location: Devaron": {
+    "value": "Devaron",
+    "label": "SWFFG.CharacterCreator.Motivation.devaron",
+  },
+  "Location: Iktotch": {
+    "value": "Iktotch",
+    "label": "SWFFG.CharacterCreator.Motivation.iktotch",
+  },
+  "Location: Lothal": {
+    "value": "Lothal",
+    "label": "SWFFG.CharacterCreator.Motivation.lothal",
+  },
+  "Location: Naboo": {
+    "value": "Naboo",
+    "label": "SWFFG.CharacterCreator.Motivation.naboo",
+  },
+  "Location: Ossus": {
+    "value": "Ossus",
+    "label": "SWFFG.CharacterCreator.Motivation.ossus",
+  },
+  "Location: Weik": {
+    "value": "Weik",
+    "label": "SWFFG.CharacterCreator.Motivation.weik",
+  },
+  "Innovation": {
+    "value": "Innovation",
+    "label": "SWFFG.CharacterCreator.Motivation.innovation",
+  },
+  "Pursuit": {
+    "value": "Pursuit",
+    "label": "SWFFG.CharacterCreator.Motivation.pursuit",
+  },
+  "Injustice": {
+    "value": "Injustice",
+    "label": "SWFFG.CharacterCreator.Motivation.injustice",
+  },
+  "Code": {
+    "value": "Code",
+    "label": "SWFFG.CharacterCreator.Motivation.code",
+  },
+  "Growth": {
+    "value": "Growth",
+    "label": "SWFFG.CharacterCreator.Motivation.growth",
+  },
+  "Mysteries": {
+    "value": "Mysteries",
+    "label": "SWFFG.CharacterCreator.Motivation.mysteries",
+  },
+  "Challenges": {
+    "value": "Challenges",
+    "label": "SWFFG.CharacterCreator.Motivation.challenges",
+  },
+  "Secrets": {
+    "value": "Secrets",
+    "label": "SWFFG.CharacterCreator.Motivation.secrets",
   },
 };
 
