40e4bb64e3ab4d99d19f6430a0e88a1df76367a2
YeNov <novakov.eo@gmail.com>
2026-02-13 12:15:58 +0100

    Also implemented a separate function for emergency repair patches

--- modules/items/item-sheet-ffg.js
+++ modules/items/item-sheet-ffg.js
@@ -549,11 +549,16 @@ export class ItemSheetFFG extends foundry.appv1.sheets.ItemSheet {
         default: true,
       });
       if (this.object.type === "gear") {
-        this.sheetoptions.register("isMedical", {
+        this.sheetoptions.register("isMedicalType", {
           name: game.i18n.localize("SWFFG.SheetOptions2.isMedical.Name"),
           hint: game.i18n.localize("SWFFG.SheetOptions2.isMedical.Hint"),
-          type: "Boolean",
-          default: false,
+          type: "Array",
+          default: "No",
+          options: {
+            "No": game.i18n.localize("SWFFG.MedicalItemType.No"),
+            "Stimpack": game.i18n.localize("SWFFG.MedicalItemType.Stimpack"),
+            "EmergencyRepairPatch": game.i18n.localize("SWFFG.MedicalItemType.EmergencyRepairPatch"),
+          },
         });
       }
       if (this.object.type === "weapon") {
